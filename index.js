ca1c60129d0a68620c01e5e8b90b189c6ada9d06
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2023-01-05 10:51:13 +0100

    changing pats

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import { scrollToTop } from './src/js/ui/scrollBtn.js';
-import { pageRouter } from './src/js/index.js';
-import { renderFooter } from './src/js/ui/pageFooter.js';
+import { scrollToTop } from '/src/js/ui/scrollBtn.js';
+import { pageRouter } from '/src/js/index.js';
+import { renderFooter } from '/src/js/ui/pageFooter.js';
 
 scrollToTop();
 pageRouter();
